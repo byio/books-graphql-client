d6f2aff59cba98240e58bbc19b877aacbefc2368
byio <ben@benyang.io>
2018-06-05 08:16:24 +0800

    add title, and add style to body of app

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div id="main">
+        <h1>Reading List</h1>
       </div>
     );
   }
