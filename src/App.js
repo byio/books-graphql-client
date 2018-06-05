b56b5f82ed1fa636b451bcfc062e74cd0f53c3a2
byio <ben@benyang.io>
2018-06-05 09:04:31 +0800

    install packages needed to set up apollo client for react, and set up apollo client on App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 // components
 import BookList from './components/BookList';
 
+// set up apollo client
+const client = new ApolloClient({
+  uri: 'localhost:2626/graphql'
+});
+
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Reading List</h1>
-        <BookList/>
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Reading List</h1>
+          <BookList/>
+        </div>
+      </ApolloProvider>
     );
   }
 }
